8ebc32a0aae4ab686bf68f61843b6978f0816e7c
Maedi Prichard <maediprichard@gmail.com>
2020-07-09 00:16:53 +1000

    Clone methods in order to branch off from main flow.

--- Reflekt.rb
+++ Reflekt.rb
@@ -1,23 +1,36 @@
+################################################################################
+# REFLEKT
+#
+# Must be defined before the class its included in with:
+# "prepend Reflekt"
+################################################################################
+
 module Reflekt
 
   def initialize(*args)
 
-    puts "--- Constructor. ---"
-
+    # Override methods.
     self.class.instance_methods(false).each do |method|
-
       self.define_singleton_method(method) do |*args|
 
-        puts "--- Method. ---"
-        p method
-        p args
+        # When method called.
+        unless @reflekt_clone == nil
+          # Reflekt on method.
+          @reflekt_clone.send(method, *args)
+        end
 
+        # Continue method flow.
         super *args
       end
 
     end
 
+    # Continue contructor flow.
     super
+
+    # Clone methods.
+    @reflekt_clone = self.clone
+
   end
 
   def self.prepended(mod)

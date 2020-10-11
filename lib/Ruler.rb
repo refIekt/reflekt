c231dbda22f589b9549e5bf10b5597866a1387d5
Maedi Prichard <maediprichard@gmail.com>
2020-10-11 14:04:33 +1100

    Fix bugs. Rename variables.

--- lib/Ruler.rb
+++ lib/Ruler.rb
@@ -10,7 +10,7 @@ require 'RuleSet'
 
 class Ruler
 
-  INPUT  = "i"
+  INPUTS  = "i"
   OUTPUT = "o"
   TYPE   = "T"
   VALUE  = "V"
@@ -19,12 +19,18 @@ class Ruler
     @rule_sets = {}
   end
 
+  ##
+  # Returns RuleSet for class/method.
+  # Returns empty array when no RuleSets exist for class/method.
+  ##
   def get(class_name, method, type)
-    # The method's ruler will not exist the first time the db generated.
-    if @rule_sets.key? class_name.to_s.to_sym
-      return @rule_sets[class_name.to_s.to_sym][method.to_s][type]
+
+    # A RuleSet for class/method will not exist the first time the db generated.
+    unless @rule_sets.key? class_name.to_s.to_sym
+      return []
     end
-    nil
+    @rule_sets[class_name.to_s.to_sym][method.to_s][type]
+
   end
 
   def set(execution, method, type, value)
@@ -33,48 +39,51 @@ class Ruler
 
   def process(class_name, method_name, controls)
 
-    inputs = get(class_name, method_name, :inputs)
+    input_rule_sets = get(class_name, method_name, :inputs)
+    output_rule_set = get(class_name, method_name, :output)
+
+    # Create a RuleSet for each control input/output.
     controls.each_with_index do |control, index|
 
-      # Create rule pools for each input.
-      control[INPUT].each_with_index do |input, index|
-        unless input.nil?
-
-          if inputs[index].nil?
-            inputs[index] = RuleSet.new()
-          end
-
-          inputs[index].process(input[TYPE], input[VALUE])
-
-        end
-      end
-
-      # Create rule pool for the output.
-      output = get(class_name, method_name, :output)
-      unless control[OUTPUT].nil?
-
-        if output.nil?
-          output = RuleSet.new()
-        end
-
-        output.process(control[OUTPUT][TYPE], control[OUTPUT][VALUE])
-
-      end
+      process_io(control, INPUTS, input_rule_sets)
+      process_io(control, OUTPUT, output_rule_set)
 
     end
 
   end
 
+  def process_io(control, io_type, rule_sets)
+
+    # Ensure outputs/input behave as arrays even if they're only one value.
+    control_type_ios = control[io_type]
+    control_type_ios = [*control_type_ios]
+
+    p control_type_ios
+
+    #control_type_ios.each_with_index do |io, index|
+
+    #  p io
+
+    #  if rule_sets[index].nil?
+    #    rule_sets[index] = RuleSet.new()
+    #  end
+
+    #  #rule_sets[index].process(io[TYPE], io[VALUE])
+
+    #end
+
+  end
+
   def train(class_name, method_name)
 
-    inputs = get(class_name, method, :inputs)
+    inputs = get(class_name, method_name, :inputs)
     inputs.each do |rule_pool|
       unless rule_pool.nil?
         rule_pool.train()
       end
     end
 
-    output = get(execution, method, :output)
+    output = get(execution, method_name, :output)
     unless output.nil?
       output.train()
     end

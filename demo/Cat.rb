bf2345a78db8878d34e039cf28fb2ccc00e993a2
Maedi Prichard <maediprichard@gmail.com>
2020-12-03 19:05:25 +1100

    Document reflekt_rename.

--- demo/Cat.rb
+++ demo/Cat.rb
@@ -2,6 +2,20 @@ require_relative 'Animal.rb'
 
 class Cat < Animal
 
+  ##
+  # It's possible to rename classes and methods without losing past reflections.
+  #
+  # Example of how to rename this class from Cat to Tiger:
+  #   reflekt_rename :class, :Tiger
+  #
+  # Example of how to rename a method from "meow()" to "roar()":
+  #   reflekt_rename :meow, :roar
+  #
+  # Then when the program runs, old reflections will be updated and reflekt_rename
+  # can be removed. However it is recommended to keep it in case another developer
+  # is working on the same codebase but using a different machine and database.
+  ##
+
   def meow(message)
 
     message = message.downcase + "eeow!"

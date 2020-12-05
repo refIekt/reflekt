d41a0801a3e7536c7bca852a4ba62affc391c6c1
maedi <maediprichard@gmail.com>
2020-12-05 22:37:53 +1100

    Fix control to use real world data.

--- lib/Control.rb
+++ lib/Control.rb
@@ -1,6 +1,8 @@
 ################################################################################
 # A shapshot of real data.
 #
+# A control's @number property will always be zero.
+#
 # @hierachy
 #   1. Execution
 #   2. Control <- YOU ARE HERE
@@ -8,9 +10,63 @@
 ################################################################################
 
 require 'Reflection'
+require 'MetaBuilder'
 
 class Control < Reflection
 
-  # Identical to a reflection except that its @number will always be zero.
+  ##
+  # Reflect on a method.
+  #
+  # Creates a shadow execution.
+  # @param *args [Dynamic] The method's arguments.
+  ##
+  def reflect(*args)
+
+    # Get aggregated rule sets.
+    input_rule_sets = @aggregator.get_input_rule_sets(@klass, @method)
+    output_rule_set = @aggregator.get_output_rule_set(@klass, @method)
+
+    # When arguments exist.
+    unless args.size == 0
+
+      # When aggregated rule sets exist.
+      unless input_rule_sets.nil?
+
+        # Validate arguments against aggregated rule sets.
+        unless @aggregator.test_inputs(args, input_rule_sets)
+          @status = :fail
+        end
+
+      end
+
+      # Create metadata for each argument.
+      # TODO: Create metadata for other inputs such as properties on the instance.
+      @inputs = MetaBuilder.create_many(args)
+
+    end
+
+    # Action method with new/old arguments.
+    begin
+
+      # Run reflection.
+      output = @clone.send(@method, *args)
+      @output = MetaBuilder.create(output)
+
+      # Validate output with aggregated control rule sets.
+      unless output_rule_set.nil?
+        unless @aggregator.test_output(output, output_rule_set)
+          @status = :fail
+        end
+      end
+
+    # When fail.
+    rescue StandardError => message
+
+      @status = :fail
+      @message = message
+
+    end
+
+  end
 
 end

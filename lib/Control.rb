f81e30d825fc96a667da4ecf5e38b7a8b7fcc9a8
maedi <maediprichard@gmail.com>
2020-12-05 22:16:34 +1100

    Simplify control reflection.

--- lib/Control.rb
+++ lib/Control.rb
@@ -8,50 +8,9 @@
 ################################################################################
 
 require 'Reflection'
-require 'MetaBuilder'
 
 class Control < Reflection
 
-  ##
-  # Get the results of the control reflection.
-  #
-  # @return [Hash] Reflection metadata.
-  ##
-  def result()
-
-    # The ID of the first execution in the ShadowStack.
-    base_id = nil
-    unless @execution.base == nil
-      base_id = @execution.base.unique_id
-    end
-
-    # Build control.
-    control = {
-      :base_id => base_id,
-      :exe_id => @execution.unique_id,
-      :ref_id => @unique_id,
-      :time => @time,
-      :class => @klass,
-      :method => @method,
-      :status => @status,
-      :message => @message,
-      :inputs => nil,
-      :output => nil,
-    }
-
-    unless @inputs.nil?
-      control[:inputs] = []
-      @inputs.each do |meta|
-        control[:inputs] << meta.result()
-      end
-    end
-
-    unless @output.nil?
-      control[:output] = @output.result()
-    end
-
-    return control
-
-  end
+  # Identical to a reflection except that its @number will always be zero.
 
 end
